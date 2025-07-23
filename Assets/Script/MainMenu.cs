2077e9fe57398da5a9d9423f95ccb74bcf09f544
ForestKum <kumweijun@gmail.com>
2025-07-23 23:20:07 +0800

    startscreen

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    private float value;
+
+    public void Start()
+    {
+        mixer.GetFloat("VOLUME", out value);
+        volumeSlider.value = value;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync("Tutorial");
@@ -12,7 +28,15 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false; // Stop Play Mode
+#else
+        Application.Quit(); // Quit game in build
+#endif
     }
 
+    public void SetVolume()
+    {
+        mixer.SetFloat("VOLUME", volumeSlider.value);
+    }
 }
