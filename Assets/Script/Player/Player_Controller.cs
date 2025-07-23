6120b34278c29296487a9a9efab82e34344daede
ForestKum <kumweijun@gmail.com>
2025-07-24 06:01:15 +0800

    SceneTransition
    havent add level 3

--- Assets/Script/Player/Player_Controller.cs
+++ Assets/Script/Player/Player_Controller.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Player_Controller : MonoBehaviour
+public class Player_Controller : Singleton<Player_Controller>
 {
     [Header("Settings")]
     [SerializeField] private float gravity = -20f;
@@ -16,6 +16,12 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private int verticalRayAmount = 4;
     [SerializeField] private int horizontalRayAmount = 4;
     [SerializeField] private Animator PlayerMove;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
 
     #region Properties
 
